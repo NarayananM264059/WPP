74fce97e343ff00b2eddf10b54c751f4d4f7ee29
NarayananM264059 <narayananmannazhi10@gmail.com>
2024-05-07 20:53:28 +0530

    Update WPP_day1ans.js

--- WPP_day1ans.js
+++ WPP_day1ans.js
@@ -1,4 +1,3 @@
-// Get the current date
 const today = new Date();
 
 // Get the day of the week
@@ -13,17 +12,15 @@ const minute = today.getMinutes();
 // Get the current second
 const second = today.getSeconds();
 
-// Determine if it's AM or PM
+// Determine if  AM or PM
 const prepand = (hour >= 12) ? " PM" : " AM";
 
-// Convert 24-hour format to 12-hour format
+// convert 24-hour format to 12-hour format
 hour = (hour >= 12) ? hour - 12 : hour;
 
 // Handle special case when hour is 0
 hour = (hour === 0) ? 12 : hour;
 
-// Display the current day
 console.log(`Today is: ${day}.`);
 
-// Display the current time
 console.log(`Current Time: ${hour}${prepand} : ${minute} : ${second}`);
